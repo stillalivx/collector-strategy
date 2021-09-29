9d4a5aaf091572d5c0b0d52f6b5d8f6efd71a25e
Jared Peña Ochoa <jaredpenaochoa@outlook.com>
2021-09-29 08:44:12 -0500

    change: improve print fmt

--- src/interface/process/list.ts
+++ src/interface/process/list.ts
@@ -1,17 +1,33 @@
 import { Colors } from "../../deps.ts";
-import { connectDatabase, closeDatabase } from "../../database/database.ts";
-
+import { closeDatabase, connectDatabase } from "../../database/database.ts";
 import SerieModel from "../../database/models/Serie.ts";
+
+import type { Serie } from "../../types.ts";
 
 async function list() {
   console.log(`💾 ${Colors.green("Obteniendo las series enlistadas...\n")}`);
 
   const database = await connectDatabase();
-  const series = await SerieModel.all();
+  const series = await SerieModel.all() as unknown as Serie[];
 
   await closeDatabase(database);
 
-  console.log(series);
+  series.forEach((serie) => {
+    console.log(
+      `${Colors.magenta(serie.id.toString())}. ${Colors.yellow(serie.name)}`,
+    );
+    console.log(
+      `${
+        Colors.cyan(!serie.description ? "Sin descripción" : serie.description)
+      }`,
+    );
+    console.log(
+      `${Colors.gray("Último número:")} ${
+        Colors.blue(serie.lastNumber.toString())
+      } (${Colors.gray(serie.lastCheck.toString())})`,
+    );
+    console.log();
+  });
 }
 
 export default list;
